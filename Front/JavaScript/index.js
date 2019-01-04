8ac34ee3cbb6481d7cd993a29241ef9518853ece
SophikoTsiklauri <46166586+SophikoTsiklauri@users.noreply.github.com>
2019-01-04 15:15:20 +0400

    Update index.js

--- Front/JavaScript/index.js
+++ Front/JavaScript/index.js
@@ -175,3 +175,16 @@ RightArrow.addEventListener("mouseleave",function(){
   setTimeout(function(){RightArrowCont.style.display = "none";}, 300);
 })
 
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 2000, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
